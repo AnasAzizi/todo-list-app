31e2beaf339dda6013781a86a2efaf76eacdcc84
Enes <enes.azizi@aartimuhendislik.com>
2024-10-14 09:25:20 +0300

    added a it design library and react icon library, added a model for editing a todo, edit and delete reducer

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,26 +1,80 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { setCheck } from '../features/TodoSlice';
+import React, { useState } from 'react';
+import { Checkbox, Button, Modal, Input } from 'antd';
+import { useDispatch } from 'react-redux';
+import { setCheck, removeTodo, editTodo } from '../features/TodoSlice';
+import { MdModeEdit, MdDelete,MdOutlineDone } from "react-icons/md";
 
 const TodoItem = ({ title, done, id }) => {
-
     const dispatch = useDispatch();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newTitle, setNewTitle] = useState(title);
 
     const handleChange = () => {
-        dispatch(setCheck(id))
-    }
+        dispatch(setCheck(id));
+    };
+
+    const handleDelete = () => {
+        dispatch(removeTodo(id));
+    };
+
+    const handleEdit = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleSave = () => {
+        if (newTitle) {
+            dispatch(editTodo({ id, title: newTitle }));
+        }
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     return (
-        <div className='flex justify-between items-center gap-12 outline-none bg-transparent border border-gray-500 p-4 w-auto min-w-96 text-white'>
-            <label htmlFor={id}>{title}</label>
-            <input
-                type='checkbox'
-                id={id}
+        <div className="w-auto min-w-96 flex justify-between items-center p-4 gap-4 bg-gray-800 rounded-md shadow-md">
+            <Checkbox
                 checked={done}
                 onChange={handleChange}
-            />
-        </div>
-    )
-}
+            >
+                <span className={done ? "line-through text-gray-400 text-xl" : "text-white text-xl"}>
+                    {title}
+                </span>
+            </Checkbox>
+            <div className="flex gap-2">
+                
+                <Button
+                    onClick={handleEdit}
+                    className="w-11 h-11 p-0 m-0 text-blue-500"
+                >
+                    <MdModeEdit className="inline-block size-5" />
+                </Button>
+                <Button
+                    type='danger'
+                    onClick={handleDelete}
+                    className="w-11 h-11 p-0 m-0 bg-white border border-red-500 text-red-500 "
+                >
+                    <MdDelete className="inline-block size-6" />
+                </Button>
+            </div>
 
-export default TodoItem
+            <Modal
+                title="Edit Todo"
+                open={isModalOpen}
+                onOk={handleSave}
+                onCancel={handleCancel}
+                okText="Save"
+                cancelText="Cancel"
+            >
+                <Input
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    placeholder="Edit your todo"
+                />
+            </Modal>
+        </div>
+    );
+};
+
+export default TodoItem;
--- src/features/TodoSlice.js
+++ src/features/TodoSlice.js
@@ -1,26 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState={
-    todoList:[],
+const initialState = {
+    todoList: [],
 };
-const TodoSlice=createSlice({
-    name:"todos",
+const TodoSlice = createSlice({
+    name: "todos",
     initialState,
-    reducers:{
-        addTodo:(state,action)=>{
-        state.todoList.push(action.payload);
+    reducers: {
+        addTodo: (state, action) => {
+            state.todoList.push(action.payload);
         },
-        setCheck:(state,action)=>{
-            state.todoList.map((todo)=>{
-                if(action.payload== todo.id){
-                    todo.done ? (todo.done=false):(todo.done=true);
+        setCheck: (state, action) => {
+            state.todoList.map((todo) => {
+                if (action.payload == todo.id) {
+                    todo.done ? (todo.done = false) : (todo.done = true);
                 }
             })
+        },
+        removeTodo: (state, action) => {
+            state.todoList = state.todoList.filter(todo => todo.id !== action.payload);
+        },
+        editTodo: (state, action) => {
+            const { id, title } = action.payload;
+            const todo = state.todoList.find((todo) => todo.id === id);
+            if (todo) {
+                todo.title = title;
+            }
         }
     }
 })
 
-export const {addTodo,setCheck}=TodoSlice.actions;
+export const { addTodo, setCheck, removeTodo, editTodo} = TodoSlice.actions;
 export default TodoSlice.reducer;
 
